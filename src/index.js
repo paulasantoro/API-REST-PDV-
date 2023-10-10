5d28cac00de190a0ad3d9842718d004ad1643808
Paula Santoro <111054052+paulasantoro@users.noreply.github.com>
2023-10-10 17:23:10 -0300

    feat: criado index e conexoes, instalado bibliotecas

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
-kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+const express = require("express");
+const app = express()
+
+const rotas = require("./rotas")
+
+
+app.use(express.json());
+app.use(rotas);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`servidor rodando na porta ${PORT}!`);
+});
