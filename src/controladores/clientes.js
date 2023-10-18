7f09acd9e4f6de5f496c56c22b4d34aa0b491b48
Larissa081208 <larissaevelinregoazevedo@gmail.com>
2023-10-18 16:47:47 -0300

    feat: inclusão de controladores "clientes.js" e "produtos.js"

---
+++ src/controladores/clientes.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt')
+const knex = require('../database/conexao')
+const jwt = require('jsonwebtoken')
+const senhaJwt = require('../jwt')
+
+const cadastrarCliente = async (req, res) => {
+
+}
+
+const editarCliente = async (req, res) => {
+
+}
+
+
+const listarClientes = async (req, res) => {
+
+}
+
+const detalharCliente = async (req, res) => {
+
+}
+
+module.exports = {
+	cadastrarCliente,
+	editarCliente,
+	listarClientes,
+	detalharCliente
+}
---
+++ src/controladores/produtos.js
@@ -0,0 +1,39 @@
+const bcrypt = require('bcrypt')
+const knex = require('../database/conexao')
+const jwt = require('jsonwebtoken')
+const senhaJwt = require('../jwt')
+
+const cadastrarProduto = async (req, res) => {
+
+}
+
+
+const editarProduto = async (req, res) => {
+
+}
+
+
+const listarProdutos = async (req, res) => {
+
+}
+
+const detalharProduto = async (req, res) => {
+
+}
+
+const excluirProduto = async (req, res) => {
+
+}
+
+const detalharProduto = async (req, res) => {
+
+}
+
+module.exports = {
+	cadastrarProduto,
+	editarProduto,
+	listarProdutos,
+	detalharProduto,
+    excluirProduto,
+    detalharProduto
+}
