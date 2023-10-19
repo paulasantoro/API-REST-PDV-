5aa83c5cd72d852afdacd21e66fb39b4e3b09f58
Larissa081208 <larissaevelinregoazevedo@gmail.com>
2023-10-19 11:32:54 -0300

    feat: add cadastrar cliente

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -4,8 +4,45 @@ const jwt = require('jsonwebtoken')
 const senhaJwt = require('../jwt')
 
 const cadastrarCliente = async (req, res) => {
+     const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body;
 
-}
+	
+	try {
+
+		if (!nome || !email || !cpf){
+			return res.status(400).json('Os campos nome, email e cpf são obrigatórios.')
+	   }
+
+		const clienteCadastrado = await knex('clientes')
+		      .where('email', email)
+			  .orWhere('cpf', cpf)
+			  .first();
+
+			  if (clienteCadastrado){
+				  return res.status(400).json('Email ou CPF já está cadastrado no sistema!')
+			}
+
+			const clienteNovo = await knex('clientes').insert({
+				nome,
+				email,
+				cpf,
+				cep,
+				rua,
+				numero,
+				bairro,
+				cidade,
+				estado
+
+			})
+
+			.returning('*');
+
+	    return res.status(201).json(clienteNovo);
+
+    }catch (error){
+		return res.status(500).json(error.message);
+	}
+};
 
 const editarCliente = async (req, res) => {
 
