75b40e82e9717e209abd080302c55ffbb9ffe953
Larissa081208 <larissaevelinregoazevedo@gmail.com>
2023-10-19 10:19:43 -0300

    feat: inserção rota listar produtos

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -48,7 +48,21 @@ const editarProduto = async (req, res) => {
 
 
 const listarProdutos = async (req, res) => {
+    const { categoria_id } = req.query;
 
+	try {
+		let query = knex ('produtos')
+				   if (categoria_id){
+					   query = query.where('categoria_id', categoria_id)
+				   }
+
+				   const resultado = await query;
+			  
+		        return res.status(200).json(resultado);
+		
+	} catch (error){
+        return res.status(400).json(error.message)
+	}
 }
 
 const detalharProduto = async (req, res) => {
