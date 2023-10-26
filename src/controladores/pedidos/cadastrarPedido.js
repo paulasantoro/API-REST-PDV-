a7767b9daae59cce67ea7df897db01e617a7e19c
Paula Santoro <111054052+paulasantoro@users.noreply.github.com>
2023-10-26 15:01:50 -0300

    feat: envio de e-mail

--- src/controladores/pedidos/cadastrarPedido.js
+++ src/controladores/pedidos/cadastrarPedido.js
@@ -1,21 +1,23 @@
 const knex = require('../../database/conexao')
+const transportador = require('../../email')
+const compiladorHtml = require('../../utils/compiladorHtml')
 
 
 const cadastrarPedido = async (req, res) => {
-	
+
     try {
 
         const { cliente_id, observacao, pedido_produtos } = req.body;
 
         //validar campos
-        if(!cliente_id || !pedido_produtos || pedido_produtos.length ===  0){
-            return res.status(400).json({ mensagem: "Campos obrigatórios"})
+        if (!cliente_id || !pedido_produtos || pedido_produtos.length === 0) {
+            return res.status(400).json({ mensagem: "Campos obrigatórios" })
         }
 
         //verificar se o cliente esta cadastrado
         const cliente = await knex('clientes').where('id', cliente_id).first();
-        if (!cliente){
-            return res.status(400).json({ mensagem: "Cliente não encontrado"})
+        if (!cliente) {
+            return res.status(400).json({ mensagem: "Cliente não encontrado" })
         }
 
         const pedido = {
@@ -23,38 +25,47 @@ const cadastrarPedido = async (req, res) => {
             observacao,
             valor_total: 0,
         };
-        
-        for (const item of pedido_produtos){
-            const produto = await knex('produtos').where('id', item.produto_id).first();
-            
 
-            if (!produto || produto.estoque < parseInt(item.quantidade_produto)){
-                return res.status(400).json({ mensagem: "produto não encontrado ou estoque insuficiente!"})
+        for (const item of pedido_produtos) {
+            const produto = await knex('produtos').where('id', item.produto_id).first();
+
+
+            if (!produto || produto.estoque < parseInt(item.quantidade_produto)) {
+                return res.status(400).json({ mensagem: "produto não encontrado ou estoque insuficiente!" })
 
             }
-           
+
             const valorProduto = parseInt(item.quantidade_produto * parseFloat(produto.valor));
             pedido.valor_total += valorProduto;
         }
-            
+
         const [pedidoId] = await knex('pedidos').insert(pedido).returning('id');
 
 
         //inserir produtos na tabela pedidos_produtos
         const pedidosProdutos = pedido_produtos.map((item) => ({
-      
+
             pedido_id: pedidoId[0],
             produto_id: item.produto_id,
             quantidade_produto: parseInt(item.quantidade_produto),
             valor_produto: item.valor
         }));
-        
+
 
         await knex('pedido_produtos').insert(pedidosProdutos);
 
-        return res.status(201).json({ mensagem: "Pedido criado coom sucesso!"})
+        // enviar e-mail
+        transportador.sendMail({
+            from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
+            to: `${cliente.nome} <${cliente.email}>`,
+            subject: 'Pedido efetuado com sucesso!',
+            text: 'Seu pedido foi registrado'
+        })
 
-    }catch(error){
+        return res.status(201).json({ mensagem: "Pedido criado coom sucesso!" })
+
+
+    } catch (error) {
         return res.status(500).json(error)
     }
 }
